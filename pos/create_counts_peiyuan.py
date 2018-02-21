3aadb0c76be3e75e1928b1bcda62f8853a49c14e
Xiangzhoujushi <peiyuantang@PeiyuandeMacBook-Pro.local>
2018-02-21 18:05:51 -0500

    joint distribution

--- pos/create_counts_peiyuan.py
+++ pos/create_counts_peiyuan.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import math
 
 # this is an example of how to parse the POS tag file and get counts
 # needed for a bigram tagger 
@@ -21,7 +22,7 @@ with open ("pos_train.txt","r") as infile:
             # 1\/2/CD means "1/2" "CD"
             # keep 1/2 as 1\/2 
             parts=wordtag.split("/")
-            tag=parts.pop()
+            tag = parts.pop()
             word="/".join(parts)
             #
             # update counters
@@ -36,5 +37,38 @@ with open ("pos_train.txt","r") as infile:
 # examples
 print ("count[NN][VB] = "+str(tag_given_tag_counts["NN"]["VB"]))
 print ("count[NN][dog] = "+str(word_given_tag_counts["NN"]["dog"]))
+
+
+# Implement the probabilities distributions for taggings and words happening at the same time.
+probability_tag_tag_dist = tag_given_tag_counts.copy()
+Probability_tag_word_dist = word_given_tag_counts.copy()
+
+sum_tag_word_pairs = 0; 
+sum_tag_tag_pairs = 0;
+
+# compute the two sums
+for key,value in tag_given_tag_counts.items():
+    # print(key)
+    sum_tag_tag_pairs+=sum(tag_given_tag_counts[key].values())
+for key,value in word_given_tag_counts.items():
+    sum_tag_word_pairs+=sum(word_given_tag_counts[key].values())
+
+print(sum_tag_tag_pairs)
+for key in tag_given_tag_counts:
+    for x in tag_given_tag_counts[key]:
+        probability_tag_tag_dist[key][x] = math.log(tag_given_tag_counts[key][x]/sum_tag_tag_pairs)
+
+for key in word_given_tag_counts:
+    for y in word_given_tag_counts[key]:
+        Probability_tag_word_dist[key][y]= math.log(word_given_tag_counts[key][y]/sum_tag_word_pairs)
+
+print (probability_tag_tag_dist["NN"]["VB"])
+
+
+
+
+
+
+
 
             
